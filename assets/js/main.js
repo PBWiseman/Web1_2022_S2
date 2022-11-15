060478bd5211e51086084d20ef4946eb2e13b944
Palin Wiseman <wisepb1@student.op.ac.nz>
2022-11-15 14:43:02 +1300

    Added fade out animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
 const BASE_URL = `https://api.unsplash.com`;
 let divs = document.querySelectorAll(".display");
 
+//This will get as many pictures as there are .display divs
 fetch(`${BASE_URL}/search/photos/?query=wine&per_page=${divs.length}&client_id=bwVpM_zDrAu_zxRv4s91vL-QY7-YfISTQNTRhefof9c`)
 .then(response => response.json())
 .then(data => {
+  //Appends an image to each selected div
   divs.forEach((div, index) => {
     let img = document.createElement("img");
     img.src = data["results"][index]["urls"]["small"];
@@ -11,30 +13,29 @@ fetch(`${BASE_URL}/search/photos/?query=wine&per_page=${divs.length}&client_id=b
   });
 })
 
+//Adds click events to open and close the menu
 let menu = document.querySelector("#popout")
 let background = document.querySelector("#background_cover")
-menu.querySelector("button").addEventListener("click", _ =>{
-  menu.style.display = "none";
-  background.style.display = "none";
-})
+
+//Main page button
 document.querySelector("button").addEventListener("click", _ =>{
   menu.style.display = "block";
   background.style.display = "block";
 })
+
+//Popout menu button
+menu.querySelector("button").addEventListener("click", _ =>{
+  menu.style.display = "none";
+  background.style.display = "none";
+})
+
+//Faded background click event
 background.addEventListener("click", _ =>{
   menu.style.display = "none";
   background.style.display = "none";
 })
 
-let inView = element => {
-  let ele = element.getBoundingClientRect();
-  if ((ele.top - window.innerHeight + 20) < 0) 
-  {
-    element.classList.add("faded_in")
-    element.classList.remove("fade_in")
-  }
-}
-
+//On scroll and on page load calls the method to check if on page
 document.querySelectorAll(".fade_in").forEach(element => {
   document.addEventListener("DOMContentLoaded", _ => {
     inView(element)
@@ -43,3 +44,18 @@ document.querySelectorAll(".fade_in").forEach(element => {
     inView(element)
   })
 })
+
+//Checks if element is on page and fades it in
+let inView = element => {
+  let ele = element.getBoundingClientRect();
+  if ((ele.top - window.innerHeight + 20) < 0) 
+  {
+    element.classList.add("faded_in")
+    element.classList.remove("fade_in")
+  }
+  if ((ele.top - window.innerHeight + 20) > -70) 
+  {
+    element.classList.add("fade_in")
+    element.classList.remove("faded_in")
+  }
+}
